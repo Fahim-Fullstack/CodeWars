85c8becf136498e477354fdb75deb03ed34a8edf
Fahim Hasan <71988931+Fahim-Fullstack@users.noreply.github.com>
2023-06-18 21:42:27 -0500

    new changes

--- Counting sheep...js
+++ Counting sheep...js
@@ -1,18 +1,16 @@
 // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-function countSheep(arrayOfSheep) {
-  let count = 0;
+
+function countSheeps(arrayOfSheep) {
+  let countSheep = 0;
   for (let i = 0; i < arrayOfSheep.length; i++) {
-    //console.log(arrayOfSheep[i]);
-    if (!null || undefined || true) {
-      return;
-    } else {
-      return (count = count + 1);
+    if (arrayOfSheep[i] === true) {
+      countSheep++;
     }
-    return count;
   }
+  return countSheep;
 }
 
-countSheep([
+countSheeps([
   true,
   true,
   true,
--- convert Number to resersed array of digits.js
+++ convert Number to resersed array of digits.js
@@ -7,3 +7,39 @@
 function digitize(n) {
   return Array.from(String(n), Number).reverse();
 }
+
+// Ways to convert a number to a string and vice versa
+
+// toString() and parseInt()
+// String() and Number()
+// unary + operator
+// multiply by 1
+// Loop
+function digitize(n) {
+  const answer = [];
+  const str = n.toString();
+
+  for (item of str) {
+    answer.unshift(parseInt(item));
+  }
+
+  return answer;
+}
+// Array.from
+function digitize(n) {
+  const str = n.toString();
+  const intConverter = (item) => parseInt(item);
+  return Array.from([...str].reverse(), intConverter);
+}
+// Let's make this solution a little bit shorter.
+
+function digitize(n) {
+  return Array.from(String(n), Number).reverse();
+}
+// Map
+function digitize(n) {
+  return ('' + n)
+    .split('')
+    .map((el) => el * 1)
+    .reverse();
+}
